49e375d844616eec64bfec91e378a2ec3894ed49
yahalomi7 <ctahrh123456789@gmail.com>
2026-01-04 13:59:02 +0200

    04.01.26

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===== תחילת בדיקת מערכת ניהול רשת חנויות =====");
 
-        // אתחול שירותים (Service)
+        // אתחול שירותים (Services)
         EmployeeService employeeService = new EmployeeService(new EmployeeRepository());
         CustomerService customerService = new CustomerService(new CustomerRepository());
 
